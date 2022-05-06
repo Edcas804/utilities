f71cf857e18a3bd90e113a30865245a6f6c5e261
Edwar C <66235321+Edcas804@users.noreply.github.com>
2022-05-06 16:19:50 -0500

    Update use.js

--- js/use.js
+++ js/use.js
@@ -1,4 +1,9 @@
 
-let style_ok = 'background: #0f0; color: #fff; padding: .5rem; border-radius: .3rem;'
-let style_error = 'background: #f00; color: #fff; padding: .5rem; border-radius: .3rem;'
-let style_info = 'background: #0af; color: #fff; padding: .5rem; border-radius: .3rem;'
+const style_ok = 'background: #0f0; color: #fff; padding: .5rem; border-radius: .3rem;'
+const style_error = 'background: #f00; color: #fff; padding: .5rem; border-radius: .3rem;'
+const style_info = 'background: #0af; color: #fff; padding: .5rem; border-radius: .3rem;'
+
+const c_ok = (text) => console.log(`%c${text}`, style_ok)
+const c_error = (text) => console.log(`%c${text}`, style_error)
+const c_info = (text) => console.log(`%c${text}`, style_info)
+  
