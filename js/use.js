bc7de784644c68a80c30166bcac77fecd19cee83
Edwar C <66235321+Edcas804@users.noreply.github.com>
2022-05-06 16:12:04 -0500

    Add files via upload

---
+++ js/use.js
@@ -0,0 +1,4 @@
+
+let style_ok = 'background: #0f0; color: #fff; padding: .5rem; border-radius: .3rem;'
+let style_error = 'background: #f00; color: #fff; padding: .5rem; border-radius: .3rem;'
+let style_info = 'background: #0af; color: #fff; padding: .5rem; border-radius: .3rem;'
